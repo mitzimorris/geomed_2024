7dea5274f5d19f782c8c858ea6b120a9f45fb32d
Mitzi Morris <mitzi212@gmail.com>
2024-09-20 19:55:46 -0400

    checkpointing, bym2 notebooks good

--- scaling_factor.R
+++ scaling_factor.R
@@ -1,7 +1,6 @@
 # computes the inverse of a sparse precision matrix
-# sub-optimal implementation - better to use INLA
 q_inv_dense <- function(Q, A = NULL) {
-  Sigma <- Matrix::solve(Q)   ## need sparse matrix solver
+  Sigma <- Matrix::solve(Q)
   if (is.null(A))
     return(Sigma)
   else {
@@ -14,18 +13,13 @@ q_inv_dense <- function(Q, A = NULL) {
 
 get_scaling_factor = function(nbs) {
     N = length(nbs)
-    adj_matrix = nb2mat(nbs,style="B")
-    # Build the adjacency matrix using edgelist
-    Q = Diagonal(counts[1], rowSums(adj_matrix)) - adj_matrix
-
     # Create ICAR precision matrix  (diag - adjacency): this is singular
+    adj_matrix = nb2mat(nbs,style="B")
     Q =  Diagonal(N, rowSums(adj_matrix)) - adj_matrix
     # Add a small jitter to the diagonal for numerical stability (optional but recommended)
     Q_pert = Q + Diagonal(N) * max(diag(Q)) * sqrt(.Machine$double.eps)
-
     # Compute the diagonal elements of the covariance matrix
     Q_inv = q_inv_dense(Q_pert, adj_matrix)
-
-    # Compute the geometric mean of the variances, which are on the diagonal of Q.inv
+    # Compute the geometric mean of the variances, which are on the diagonal of Q_inv
     return(exp(mean(log(diag(Q_inv)))))
 }

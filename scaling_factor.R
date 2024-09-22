89b1f86615d7b257240796ae30f615ce39a7cad8
Mitzi Morris <mitzi212@gmail.com>
2024-09-22 16:18:02 -0400

    minor corrections

--- scaling_factor.R
+++ scaling_factor.R
@@ -1,6 +1,7 @@
 # computes the inverse of a sparse precision matrix
+# sub-optimal implementation - better to use INLA
 q_inv_dense <- function(Q, A = NULL) {
-  Sigma <- Matrix::solve(Q)
+  Sigma <- Matrix::solve(Q)   ## need sparse matrix solver
   if (is.null(A))
     return(Sigma)
   else {
@@ -13,13 +14,18 @@ q_inv_dense <- function(Q, A = NULL) {
 
 get_scaling_factor = function(nbs) {
     N = length(nbs)
-    # Create ICAR precision matrix  (diag - adjacency): this is singular
     adj_matrix = nb2mat(nbs,style="B")
+    # Build the adjacency matrix using edgelist
+    Q = Diagonal(counts[1], rowSums(adj_matrix)) - adj_matrix
+
+    # Create ICAR precision matrix  (diag - adjacency): this is singular
     Q =  Diagonal(N, rowSums(adj_matrix)) - adj_matrix
     # Add a small jitter to the diagonal for numerical stability (optional but recommended)
     Q_pert = Q + Diagonal(N) * max(diag(Q)) * sqrt(.Machine$double.eps)
+
     # Compute the diagonal elements of the covariance matrix
     Q_inv = q_inv_dense(Q_pert, adj_matrix)
-    # Compute the geometric mean of the variances, which are on the diagonal of Q_inv
+
+    # Compute the geometric mean of the variances, which are on the diagonal of Q.inv
     return(exp(mean(log(diag(Q_inv)))))
 }
